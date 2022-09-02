f54848b92a4b02515b73700c9a93b5402a6065cf
Andrei Ioniță <hi@andrei.io>
2022-09-02 18:35:19 +0100

    init azure

---
+++ azure/main.tf
@@ -0,0 +1,17 @@
+module "web_app" {
+  source = "./modules/web_app"
+
+  # Input Variables
+  docker_tag        = var.docker_tag
+  hostname          = var.hostname
+  project_slug      = var.project_slug
+  region            = var.region
+  debug_mode        = var.debug_mode
+  mail_host         = var.mail_host
+  mail_port         = var.mail_port
+  mail_username     = var.mail_username
+  mail_password     = var.mail_password
+  mail_encryption   = var.mail_encryption
+  mail_from_address = var.mail_from_address
+  admin_email       = var.admin_email
+}
---
+++ azure/modules/web_app/variables.tf
@@ -0,0 +1,59 @@
+variable "docker_tag" {
+  description = "Docker image tag"
+  type        = string
+}
+
+variable "project_slug" {
+  description = "Project slug"
+  type        = string
+}
+
+variable "hostname" {
+  description = "Domain for web app"
+  type        = string
+}
+
+variable "debug_mode" {
+  description = "Is debug mode enabled"
+  type        = bool
+}
+
+variable "region" {
+  description = "Region where to deploy resources"
+  type        = string
+}
+
+variable "mail_host" {
+  description = "SMTP host"
+  type        = string
+}
+
+variable "mail_port" {
+  description = "SMTP port"
+  type        = string
+}
+
+variable "mail_username" {
+  description = "SMTP username"
+  type        = string
+}
+
+variable "mail_password" {
+  description = "SMTP password"
+  type        = string
+}
+
+variable "mail_encryption" {
+  description = "SMTP encryption"
+  type        = bool
+}
+
+variable "mail_from_address" {
+  description = "Mail from address"
+  type        = string
+}
+
+variable "admin_email" {
+  description = "Email address of initial admin account"
+  type        = string
+}
