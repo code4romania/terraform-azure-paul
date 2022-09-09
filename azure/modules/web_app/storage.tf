41cca6c92338629c583e30dfc69a6c037304c7ba
Andrei Ioniță <hi@andrei.io>
2022-09-09 13:06:14 +0100

    prevent stateful resources destruction

--- azure/modules/web_app/storage.tf
+++ azure/modules/web_app/storage.tf
@@ -27,10 +27,18 @@ resource "azurerm_storage_account" "storage_account" {
   identity {
     type = "SystemAssigned"
   }
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "azurerm_storage_container" "storage_container" {
   name                  = local.storage_config.container_name
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "blob"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
